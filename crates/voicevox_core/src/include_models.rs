34b4ddfb464343f4264bd17091ee2e8b40307005
Hiroshiba <hihokaruta@gmail.com>
2023-01-03 15:48:43 +0900

    モデルをいっぱいにする

--- crates/voicevox_core/src/include_models.rs
+++ crates/voicevox_core/src/include_models.rs
@@ -13,4 +13,186 @@
         "/model/decode.onnx"
     )),
     },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
+    Model{
+        predict_duration_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_duration.onnx"
+    )),
+        predict_intonation_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"),
+        "/model/predict_intonation.onnx"
+    )),
+        decode_model: include_bytes!(concat!(
+        env!("CARGO_WORKSPACE_DIR"), 
+        "/model/decode.onnx"
+    )),
+    },
 ]
