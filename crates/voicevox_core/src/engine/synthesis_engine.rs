8dda70932e16cb05bdbf30aee28cf3a344f8aed4
y-chan <y-chan@y-chan.dev>
2023-01-06 11:48:03 +0900

    lint

--- crates/voicevox_core/src/engine/synthesis_engine.rs
+++ crates/voicevox_core/src/engine/synthesis_engine.rs
@@ -5,7 +5,7 @@ use derive_new::new;
 // use super::full_context_label::Utterance;
 use super::open_jtalk::OpenJtalk;
 use super::*;
-use crate::numerics::F32Ext as _;
+// use crate::numerics::F32Ext as _;
 use crate::InferenceCore;
 
 #[allow(dead_code)]
